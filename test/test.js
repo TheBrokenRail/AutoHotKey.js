5fe317ffc723605aab69c620be9f4b52d59599d0
TheBrokenRail <connor24nolan@live.com>
2018-02-18 10:36:30 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 require('../index.js').init('output');
 
-If(WinExist('Documents'), function () {
+If(WinExist('"Documents"'), function () {
   on('^t', function () {
-    If(WinExist('Untitled - Notepad'), function () {
+    If(WinExist('"Untitled - Notepad"'), function () {
       send('Notepad Open');
     }).Else(function () {
       send('Notepad Not Open');
