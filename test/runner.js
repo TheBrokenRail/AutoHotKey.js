1a1607304de44a7f2e450e32af783112dfbefffd
TheBrokenRail <connor24nolan@live.com>
2018-02-18 11:49:36 -0500

    Update runner.js

--- test/runner.js
+++ test/runner.js
@@ -1,16 +1,14 @@
 const fs = require('fs');
 const child_process = require('child_process');
 
-console.log('JavaScript Script:');
-console.log(fs.readFileSync(__dirname + '/test.js', 'utf8'));
-console.log('Expected AHK Script:');
-console.log(fs.readFileSync(__dirname + '/expected.ahk', 'utf8'));
-child_process.spawnSync('node', [__dirname + '/test.js'], {stdio: 'inherit'});
-console.log('Output AHK Script:');
-console.log(fs.readFileSync(__dirname + '/output.ahk', 'utf8'));
-if (fs.readFileSync(__dirname + '/output.ahk', 'utf8') === fs.readFileSync(__dirname + '/expected.ahk', 'utf8')) {
-  console.log('Success!');
-} else {
-  console.log('Failure! Output And Expected AHK Files Do Not Match!');
-  process.exit(1);
-}
+module.exports.test = function (test) {
+  console.log('JavaScript Script:');
+  console.log(fs.readFileSync(__dirname + '/test.js', 'utf8'));
+  console.log('Expected AHK Script:');
+  console.log(fs.readFileSync(__dirname + '/expected.ahk', 'utf8'));
+  child_process.spawnSync('node', [__dirname + '/test.js'], {stdio: 'inherit'});
+  console.log('Output AHK Script:');
+  console.log(fs.readFileSync(__dirname + '/output.ahk', 'utf8'));
+  test.strictEqual(fs.readFileSync(__dirname + '/output.ahk', 'utf8'), fs.readFileSync(__dirname + '/expected.ahk', 'utf8'), 'Actual and Expected Scripts Should Be The Same');
+  test.done();
+};
