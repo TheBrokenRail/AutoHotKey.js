cebae4cf8b0aab91fa6c3090376f2c1f97b726a3
TheBrokenRail <connor24nolan@live.com>
2018-02-19 11:14:58 -0500

    Update doc.js

--- doc.js
+++ doc.js
@@ -1,14 +1,15 @@
 const commands = require('./commands.json');
 const fs = require('fs');
 
-fs.writeFileSync('doc.md', '# Documentation\n\n');
+fs.mkdirSync('gh-pages');
+fs.writeFileSync('gh-pages/doc.md', '# Documentation\n\n');
 for (let x in commands) {
-  fs.appendFileSync('doc.md', '## ' + x + '\n');
-  fs.appendFileSync('doc.md', '```js\n');
-  fs.appendFileSync('doc.md', x + '( ... );\n');
-  fs.appendFileSync('doc.md', '```\n\n');
-  fs.appendFileSync('doc.md', '#### Which Outputs\n');
-  fs.appendFileSync('doc.md', '```ahk\n');
-  fs.appendFileSync('doc.md', commands[x].replace(new RegExp('%', 'g'), '...') + '\n');
-  fs.appendFileSync('doc.md', '```\n\n');
+  fs.appendFileSync('gh-pages/doc.md', '## ' + x + '\n');
+  fs.appendFileSync('gh-pages/doc.md', '```js\n');
+  fs.appendFileSync('gh-pages/doc.md', x + '( ... );\n');
+  fs.appendFileSync('gh-pages/doc.md', '```\n\n');
+  fs.appendFileSync('gh-pages/doc.md', '#### Which Outputs\n');
+  fs.appendFileSync('gh-pages/doc.md', '```ahk\n');
+  fs.appendFileSync('gh-pages/doc.md', commands[x].replace(new RegExp('%', 'g'), '...') + '\n');
+  fs.appendFileSync('gh-pages/doc.md', '```\n\n');
 }
