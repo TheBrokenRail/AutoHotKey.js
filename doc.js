c62b47b2e8549e474ec932cb8a811f78b96af8dc
TheBrokenRail <connor24nolan@live.com>
2018-02-19 12:48:56 -0500

    Update doc.js

--- doc.js
+++ doc.js
@@ -8,24 +8,24 @@ fs.appendFileSync('gh-pages/doc.md', fs.readFileSync('generalDoc.md', 'utf8'));
 
 fs.appendFileSync('gh-pages/doc.md', '# Commands/Functions\n\n');
 for (let x in commandsJson) {
-  fs.appendFileSync('gh-pages/doc.md', '## ' + x + '\n');
-  fs.appendFileSync('gh-pages/doc.md', '```js\n');
-  fs.appendFileSync('gh-pages/doc.md', x + '(...);\n');
-  fs.appendFileSync('gh-pages/doc.md', '```\n\n');
-  fs.appendFileSync('gh-pages/doc.md', '#### Which Outputs\n');
-  fs.appendFileSync('gh-pages/doc.md', '```ahk\n');
-  fs.appendFileSync('gh-pages/doc.md', commandsJson[x].replace(new RegExp('%', 'g'), '...') + '\n');
-  fs.appendFileSync('gh-pages/doc.md', '```\n\n');
+  fs.appendFileSync('gh-pages/doc.md', '## ' + x + '\n' +
+    'gh-pages/doc.md', '```js\n' +
+    'gh-pages/doc.md', x + '(...);\n' +
+    'gh-pages/doc.md', '```\n\n' +
+    'gh-pages/doc.md', '#### Which Outputs\n' +
+    'gh-pages/doc.md', '```ahk\n' +
+    'gh-pages/doc.md', commandsJson[x].replace(new RegExp('%', 'g'), '...') + '\n' +
+    'gh-pages/doc.md', '```\n\n');
 }
 
 fs.appendFileSync('gh-pages/doc.md', '# Conditions\n\n');
 for (let x in conditionsJson) {
-  fs.appendFileSync('gh-pages/doc.md', '## ' + x + '\n');
-  fs.appendFileSync('gh-pages/doc.md', '```js\n');
-  fs.appendFileSync('gh-pages/doc.md', x + '(...);\n');
-  fs.appendFileSync('gh-pages/doc.md', '```\n\n');
-  fs.appendFileSync('gh-pages/doc.md', '#### Which Outputs\n');
-  fs.appendFileSync('gh-pages/doc.md', '```ahk\n');
-  fs.appendFileSync('gh-pages/doc.md', conditionsJson[x].replace(new RegExp('%', 'g'), '...') + '\n');
-  fs.appendFileSync('gh-pages/doc.md', '```\n\n');
+  fs.appendFileSync('gh-pages/doc.md', '## ' + x + '\n' +
+    'gh-pages/doc.md', '```js\n' +
+    'gh-pages/doc.md', x + '(...);\n' +
+    'gh-pages/doc.md', '```\n\n' +
+    'gh-pages/doc.md', '#### Which Outputs\n' +
+    'gh-pages/doc.md', '```ahk\n' +
+    'gh-pages/doc.md', conditionsJson[x].replace(new RegExp('%', 'g'), '...') + '\n' +
+    'gh-pages/doc.md', '```\n\n');
 }
