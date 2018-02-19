3ed270deb73778e16faf687ffba870904b3f019b
TheBrokenRail <connor24nolan@live.com>
2018-02-19 12:16:11 -0500

    Update doc.js

--- doc.js
+++ doc.js
@@ -2,11 +2,11 @@ const commandsJson = require('./commands.json');
 const conditionsJson = require('./conditions.json');
 const fs = require('fs');
 
-fs.writeFileSync('gh-pages/doc.md', '# Documentation\n\n');
-fs.writeFileSync('gh-pages/doc.md', '# General\n\n');
+fs.writeFileSync('gh-pages/doc.md', '');
+fs.appendFileSync('gh-pages/doc.md', '# General\n\n');
 fs.appendFileSync('gh-pages/doc.md', fs.readFileSync('generalDoc.md', 'utf8'));
 
-fs.writeFileSync('gh-pages/doc.md', '# Commands/Functions\n\n');
+fs.appendFileSync('gh-pages/doc.md', '# Commands/Functions\n\n');
 for (let x in commandsJson) {
   fs.appendFileSync('gh-pages/doc.md', '## ' + x + '\n');
   fs.appendFileSync('gh-pages/doc.md', '```js\n');
@@ -18,7 +18,7 @@ for (let x in commandsJson) {
   fs.appendFileSync('gh-pages/doc.md', '```\n\n');
 }
 
-fs.writeFileSync('gh-pages/doc.md', '# Conditions\n\n');
+fs.appendFileSync('gh-pages/doc.md', '# Conditions\n\n');
 for (let x in conditionsJson) {
   fs.appendFileSync('gh-pages/doc.md', '## ' + x + '\n');
   fs.appendFileSync('gh-pages/doc.md', '```js\n');
