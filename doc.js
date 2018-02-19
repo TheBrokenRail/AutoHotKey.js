53518f09048155efcb450b2fa1f6d2bf6bcf1d54
TheBrokenRail <connor24nolan@live.com>
2018-02-19 11:51:57 -0500

    Update doc.js

--- doc.js
+++ doc.js
@@ -2,6 +2,7 @@ const commands = require('./commands.json');
 const fs = require('fs');
 
 fs.writeFileSync('gh-pages/doc.md', '# Documentation\n\n');
+fs.appendFileSync('gh-pages/doc.md', fs.readFileSync('generalDoc.md', 'utf8'));
 for (let x in commands) {
   fs.appendFileSync('gh-pages/doc.md', '## ' + x + '\n');
   fs.appendFileSync('gh-pages/doc.md', '```js\n');
