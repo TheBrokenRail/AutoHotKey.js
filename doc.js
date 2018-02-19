86c16b400ba817e4b4f1dc2466d1e7e540649be2
TheBrokenRail <connor24nolan@live.com>
2018-02-19 12:12:19 -0500

    Update doc.js

--- doc.js
+++ doc.js
@@ -1,17 +1,31 @@
 const commandsJson = require('./commands.json');
 const conditionsJson = require('./conditions.json');
-const json = Object.assign({}, commandsJson, conditionsJson);
 const fs = require('fs');
 
 fs.writeFileSync('gh-pages/doc.md', '# Documentation\n\n');
+fs.writeFileSync('gh-pages/doc.md', '# General\n\n');
 fs.appendFileSync('gh-pages/doc.md', fs.readFileSync('generalDoc.md', 'utf8'));
-for (let x in json) {
+
+fs.writeFileSync('gh-pages/doc.md', '# Commands/Functions\n\n');
+for (let x in commandsJson) {
   fs.appendFileSync('gh-pages/doc.md', '## ' + x + '\n');
   fs.appendFileSync('gh-pages/doc.md', '```js\n');
   fs.appendFileSync('gh-pages/doc.md', x + '(...);\n');
   fs.appendFileSync('gh-pages/doc.md', '```\n\n');
   fs.appendFileSync('gh-pages/doc.md', '#### Which Outputs\n');
   fs.appendFileSync('gh-pages/doc.md', '```ahk\n');
-  fs.appendFileSync('gh-pages/doc.md', json[x].replace(new RegExp('%', 'g'), '...') + '\n');
+  fs.appendFileSync('gh-pages/doc.md', commandsJson[x].replace(new RegExp('%', 'g'), '...') + '\n');
+  fs.appendFileSync('gh-pages/doc.md', '```\n\n');
+}
+
+fs.writeFileSync('gh-pages/doc.md', '# Conditions\n\n');
+for (let x in conditionsJson) {
+  fs.appendFileSync('gh-pages/doc.md', '## ' + x + '\n');
+  fs.appendFileSync('gh-pages/doc.md', '```js\n');
+  fs.appendFileSync('gh-pages/doc.md', x + '(...);\n');
+  fs.appendFileSync('gh-pages/doc.md', '```\n\n');
+  fs.appendFileSync('gh-pages/doc.md', '#### Which Outputs\n');
+  fs.appendFileSync('gh-pages/doc.md', '```ahk\n');
+  fs.appendFileSync('gh-pages/doc.md', conditionsJson[x].replace(new RegExp('%', 'g'), '...') + '\n');
   fs.appendFileSync('gh-pages/doc.md', '```\n\n');
 }
