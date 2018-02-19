17f59f994ce94f705c3cca076236f1cb66cdc7af
TheBrokenRail <connor24nolan@live.com>
2018-02-19 13:06:00 -0500

    Update doc.js

--- doc.js
+++ doc.js
@@ -9,23 +9,23 @@ fs.appendFileSync('gh-pages/doc.md', fs.readFileSync('generalDoc.md', 'utf8'));
 fs.appendFileSync('gh-pages/doc.md', '# Commands/Functions\n\n');
 for (let x in commandsJson) {
   fs.appendFileSync('gh-pages/doc.md', '## ' + x + '\n' +
-    'gh-pages/doc.md', '```js\n' +
-    'gh-pages/doc.md', x + '(...);\n' +
-    'gh-pages/doc.md', '```\n\n' +
-    'gh-pages/doc.md', '#### Which Outputs\n' +
-    'gh-pages/doc.md', '```ahk\n' +
-    'gh-pages/doc.md', commandsJson[x].replace(new RegExp('%', 'g'), '...') + '\n' +
-    'gh-pages/doc.md', '```\n\n');
+    '```js\n' +
+    x + '(...);\n' +
+    '```\n\n' +
+    '#### Which Outputs\n' +
+    '```ahk\n' +
+    commandsJson[x].replace(new RegExp('%', 'g'), '...') + '\n' +
+    '```\n\n');
 }
 
 fs.appendFileSync('gh-pages/doc.md', '# Conditions\n\n');
 for (let x in conditionsJson) {
   fs.appendFileSync('gh-pages/doc.md', '## ' + x + '\n' +
-    'gh-pages/doc.md', '```js\n' +
-    'gh-pages/doc.md', x + '(...);\n' +
-    'gh-pages/doc.md', '```\n\n' +
-    'gh-pages/doc.md', '#### Which Outputs\n' +
-    'gh-pages/doc.md', '```ahk\n' +
-    'gh-pages/doc.md', conditionsJson[x].replace(new RegExp('%', 'g'), '...') + '\n' +
-    'gh-pages/doc.md', '```\n\n');
+    '```js\n' +
+    x + '(...);\n' +
+    '```\n\n' +
+    '#### Which Outputs\n' +
+    '```ahk\n' +
+    conditionsJson[x].replace(new RegExp('%', 'g'), '...') + '\n' +
+    '```\n\n');
 }
