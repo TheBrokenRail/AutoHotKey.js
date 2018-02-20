aff04c0a28fad7057eb6913ecf2c456dbe28fb2b
TheBrokenRail <connor24nolan@live.com>
2018-02-20 17:52:16 -0500

    Update runner.js

--- test/runner.js
+++ test/runner.js
@@ -1,10 +1,17 @@
 const assert = require('assert');
+const autohotkey = require('../index.js');
 const fs = require('fs');
 
 describe('AutoHotKey.js', function () {
   it('The Output And Expected AHKs Should Be Equal', function () {
     const output = require('./test.js');
-    assert.strictEqual(output.getName(), 'output.ahk', 'Name Should Be output.ahk');
-    assert.strictEqual(output.getText(), fs.readFileSync(__dirname + '/expected.ahk', 'utf8'), 'Actual and Expected Scripts Should Be The Same');
+    assert.strictEqual(output.getText(), fs.readFileSync(__dirname + '/expected.ahk', 'utf8'));
+  });
+  describe('Script.getName()', function () {
+    it('The Name Should be Correct', function () {
+      var script = new autohotkey.Script();
+      autohotkey.init('Name', script);
+      assert.strictEqual(script.getName(), 'Name.ahk');
+    });
   });
 });
